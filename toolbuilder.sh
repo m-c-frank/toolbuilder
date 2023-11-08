9d9559602595255bb526496212bbff6958704b39
Martin Christoph Frank <martinchristophfrank@gmail.com>
2023-11-08 20:23:41 +0100

    updated toolbuilder

--- toolbuilder.sh
+++ toolbuilder.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # toolbuilder.sh
-# This script parses a README.md file and creates files based on the code blocks.
+# This script parses a specified README.md file and creates files based on the code blocks.
+
+show_help() {
+    echo "Usage: toolbuilder [-h] [-i INPUT_PATH] [-o OUTPUT_PATH]"
+    echo "  -h                Display this help message and exit."
+    echo "  -i INPUT_PATH     Specify the path to the README.md file to parse."
+    echo "  -o OUTPUT_PATH    Specify the root directory where files will be created."
+}
 
 parse_readme() {
     local readme_path="$1"
+    local output_path="$2"
     local in_code_block=false
     local file_path=""
     local content=()
@@ -11,7 +19,7 @@ parse_readme() {
     while IFS= read -r line || [[ -n "$line" ]]; do
         if [[ "$line" =~ ^##\ .\/ ]]; then
             if [ "$in_code_block" = true ] && [ -n "$file_path" ]; then
-                extract_and_write "$file_path" "${content[@]}"
+                extract_and_write "$output_path/$file_path" "${content[@]}"
                 content=()
             fi
             file_path=$(echo "$line" | sed 's/^## \(.*\)/\1/')
@@ -19,7 +27,7 @@ parse_readme() {
         elif [[ "$line" == '```'* ]]; then
             in_code_block=!$in_code_block
             if [ "$in_code_block" = false ] && [ -n "$file_path" ]; then
-                extract_and_write "$file_path" "${content[@]}"
+                extract_and_write "$output_path/$file_path" "${content[@]}"
                 file_path=""
                 content=()
             fi
@@ -36,11 +44,41 @@ extract_and_write() {
     printf "%s\n" "${content[@]}" > "$path"
 }
 
+# Default paths
+input_path="README.md"
+output_path="."
+
+# Parse command line options
+while getopts ":hi:o:" opt; do
+    case ${opt} in
+        h )
+            show_help
+            exit 0
+            ;;
+        i )
+            input_path=$OPTARG
+            ;;
+        o )
+            output_path=$OPTARG
+            ;;
+        \? )
+            echo "Invalid Option: -$OPTARG" 1>&2
+            show_help
+            exit 1
+            ;;
+        : )
+            echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
 # Main execution
-if [ -f "README.md" ]; then
-    parse_readme "README.md"
-else
-    echo "Error: README.md not found."
+if [ ! -f "$input_path" ]; then
+    echo "Error: README.md not found at path: $input_path"
     exit 1
 fi
+
+parse_readme "$input_path" "$output_path"
 
