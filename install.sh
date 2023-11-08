58d89c1a0dd7d6942cab9f8cc7470c4811f6870d
Martin Christoph Frank <martinchristophfrank@gmail.com>
2023-11-08 20:16:38 +0100

    hopefully fixed path

--- install.sh
+++ install.sh
@@ -15,30 +15,28 @@ curl -s "$REPO_URL" -o "$INSTALL_DIR/$TOOL_NAME"
 # Make the toolbuilder script executable
 chmod +x "$INSTALL_DIR/$TOOL_NAME"
 
-# Check if the install directory is in PATH
+# Add the install directory to PATH if it's not already there
 if ! echo "$PATH" | grep -q "$INSTALL_DIR"; then
-    # Add the install directory to PATH
     echo "Adding $INSTALL_DIR to PATH"
-    
-    # Detect profile file based on current shell
+
+    # Detect the shell and choose the appropriate profile file
     if [ -n "$BASH_VERSION" ]; then
         PROFILE="$HOME/.bashrc"
+        echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> "$PROFILE"
     elif [ -n "$ZSH_VERSION" ]; then
         PROFILE="$HOME/.zshrc"
+        echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> "$PROFILE"
     elif [ -n "$FISH_VERSION" ]; then
-        PROFILE="$HOME/.config/fish/config.fish"
-        echo "set -U fish_user_paths $INSTALL_DIR \$fish_user_paths" >> "$PROFILE"
+        # For fish shell, use fish_user_paths universal variable
+        fish -c "set -U fish_user_paths $INSTALL_DIR \$fish_user_paths"
     else
         echo "Unknown shell. Please manually add $INSTALL_DIR to your PATH."
         exit 1
     fi
 
-    # For bash and zsh, update the profile file
-    if [ -z "$FISH_VERSION" ]; then
-        echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> "$PROFILE"
-    fi
-
     echo "Please restart your terminal or source your profile to update your PATH."
+else
+    echo "$INSTALL_DIR is already in PATH"
 fi
 
 echo "ToolBuilder installation complete."
